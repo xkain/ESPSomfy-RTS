c7775765e103d88c018d66bfc50bdbb7bb3fd4f7
xkain <xkain@outlook.fr>
2026-02-16 00:27:46 +0100

    Added a  status MAJ to Firmware page and improved button clarity for better UX.

--- data/index.js
+++ data/index.js
@@ -1554,7 +1554,7 @@ var security = new Security();
 
 class General {
     initialized = false;
-    appVersion = 'v2.4.8';
+    appVersion = 'v2.4.9';
     reloadApp = false;
     init() {
         if (this.initialized) return;
