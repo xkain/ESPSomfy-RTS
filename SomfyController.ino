dbe49144f893f7296a59d249cd8b1b6c99047ac6
xkain <xkain@outlook.fr>
2026-02-23 17:36:34 +0100

    v2.4.8: Added Hard Reset logic and updated default configuration

--- SomfyController.ino
+++ SomfyController.ino
@@ -30,6 +30,7 @@ void setup() {
   Serial.println("Mounting File System...");
   if(LittleFS.begin()) Serial.println("File system mounted successfully");
   else Serial.println("Error mounting file system");
+  handlePowerCycleReset();
   settings.begin();
   if(WiFi.status() == WL_CONNECTED) WiFi.disconnect(true);
   delay(10);
